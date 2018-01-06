3aa9cf38d747e8661b76d7ba1014b36e88ca7ab2
Francesco Casula <fra.casula@gmail.com>
2018-01-06 13:22:50 +0000

    main now reads from a log.txt file

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
-import "./slack"
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"./slack"
+)
 
 func main() {
+	file, err := os.Open("./log.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
 	slackAPI := slack.API{
 		Endpoint: slack.Endpoint{"/T8NNCR01G/B8NU7G8PM/9qSAhfSTSOvOjlVGdZCi1F2n"},
 	}
 
-	slackAPI.SendMessage("Ciao!")
+	for scanner.Scan() {
+		err := slackAPI.SendMessage(scanner.Text())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
