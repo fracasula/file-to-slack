5c0ac28cb277168aa1bb81f8cb65fbbe3379b931
Francesco Casula <fra.casula@gmail.com>
2018-01-06 17:43:17 +0000

    Better error messages

--- slack/slack.go
+++ slack/slack.go
@@ -33,17 +33,17 @@ func (slack *API) SendMessage(message string) error {
 	body, err := json.Marshal(jsonMessage{message})
 
 	if err != nil {
-		return err
+		return fmt.Errorf("marshalling failed with %s: %v", message, err)
 	}
 
 	resp, err := http.Post(slack.baseURL+slack.URL, "application/json", bytes.NewReader(body))
 
 	if err != nil {
-		return err
+		return fmt.Errorf("request failed: %v", err)
 	}
 
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("Something went wrong, Slack did not respond with a 200 OK")
+		return fmt.Errorf("Slack API did not respond with a 200 OK")
 	}
 
 	defer resp.Body.Close()
